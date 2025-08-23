2160bea6bc39f9c8a3d9b6bdf1da8d94dbcd688a
gsigor84 <gsigor84@gmail.com>
2025-08-23 13:34:24 +0100

    Use CommonJS next.config.js; remove next.config.mjs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Don’t fail the build on ESLint errors in CI
     ignoreDuringBuilds: true,
   },
 };
-
 module.exports = nextConfig;
